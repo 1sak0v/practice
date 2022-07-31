ef170fb5f8fcaefd2f6f621953a56175ca2d7cc1
Anton <zeta664433@gmail.com>
2022-07-31 18:14:15 +0300

    lesson 19 first project

---
+++ index.js
@@ -0,0 +1,20 @@
+const numbersOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const lastFilm = prompt("Один из последих просмотренных фильмов?", "");
+const filmRating = prompt("На сколько его оцените?", "");
+
+const lastFilm2 = prompt("Один из последих просмотренных фильмов?", "");
+const filmRating2 = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[lastFilm] = filmRating;
+personalMovieDB.movies[lastFilm2] = filmRating2;
+
+console.log(personalMovieDB);
