dba84d77bc7f566eda96758bccf7afe5487f6a29
Anton Vyun <zeta664433@gmail.com>
2022-08-03 12:03:59 +0300

    update method writeYourGeners

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const personalMovieDB = {
     actors: {},
     geners: [],
     privat: false,
-    start: function() {
+    start: function () {
         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
         while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
             personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
         }
     },
-    rememberMyFilms: function() {
+    rememberMyFilms: function () {
         for (let i = 0; i < 2; i++) {
             let lastFilm = prompt("Один из последих просмотренных фильмов?", "").trim();
             let filmRating = prompt("На сколько его оцените?", "").trim();
-        
+
             if (lastFilm !== "" && filmRating !== "" && lastFilm !== null && filmRating !== null && lastFilm.length < 50) {
                 personalMovieDB.movies[lastFilm] = filmRating;
             } else {
@@ -22,7 +22,7 @@ const personalMovieDB = {
             }
         }
     },
-    detectPersonalLevel: function() {
+    detectPersonalLevel: function () {
         if (personalMovieDB.count < 10) {
             console.log("Просмотрено довольно мало фильмов");
         } else if (personalMovieDB.count <= 30) {
@@ -33,26 +33,28 @@ const personalMovieDB = {
             console.log("Произошла ошибка");
         }
     },
-    showMyDB: function() {
+    showMyDB: function () {
         if (!personalMovieDB.privat) {
             console.log(personalMovieDB);
         }
     },
-    writeYourGeners: function() {
-        for (let i = 1; i <= 3; i++) {
-            const gener = prompt(`Ваш любимый жанр под номером ${i}?`, "").trim();
-            if (gener !== "" && gener !== null) {
-                personalMovieDB.geners[i-1] = gener;
-            } else {
+    writeYourGeners: function () {
+        for (let i = 1; i < 2; i++) {
+            const geners = prompt("Введите ваши любимые жанры через запятую?", "");
+            if (geners === "" || geners === null || geners.trim() === "") {
+                console.log("Неверный ввод");
                 i--;
+            } else {
+                personalMovieDB.geners = geners.split(",");
             }
+
         }
         personalMovieDB.geners.forEach((gener, index) => {
-            const res = `Любимый жанр ${index + 1} - это ${gener}`
-            console.log(res)
-        })
+            const res = `Любимый жанр ${index + 1} - это ${gener}`;
+            console.log(res);
+        });
     },
-    toggleVisibleMyDB: function() {
+    toggleVisibleMyDB: function () {
         personalMovieDB.privat = !personalMovieDB.privat;
     }
 };
